482b0fa939791a38049b32c7d4020d00eafd72b6
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-07-18 22:36:43 +0330

    feat : create HeaderApp component

---
+++ src/components/Home/HeaderApp.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header-content">
+        <div class="header-content-brand">
+          <div class="title">
+            <h1>رستوران من</h1>
+            <h3>غذاهای لذیذ ما</h3>
+          </div>
+          <div class="header-logo">
+            <i class="fa fa-utensils"></i>
+          </div>
+        </div>
+        <div class="header-banner">
+          <h1>خوش آمدید</h1>
+          <h1>غذاهای ما را امتحان کنید</h1>
+          <button>رزرو میز</button>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
