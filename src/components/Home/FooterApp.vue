3c92e318a9d373717716f816d415fecbd30d66d2
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-07-18 22:34:33 +0330

    feat : create FooterApp component

---
+++ src/components/Home/FooterApp.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <div class="container">
+      <div class="footer">
+        <ul>
+          <li v-for="(item,index) in socials" :key="index">
+            <a href="#"><i :class="item.icon"></i></a>
+          </li>        
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+const socials = [
+  {icon:'fab fa-youtube'},
+  {icon:'fab fa-instagram'},
+  {icon:'fab fa-telegram'},
+  {icon:'fab fa-linkedin-in'},
+]
+</script>
+<style lang="scss" scoped></style>
