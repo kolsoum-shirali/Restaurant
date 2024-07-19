cfdaab6295254af3f36e66a1a9a5c435008cdd89
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-07-19 12:29:56 +0330

    feat : using store for manage orders

--- src/components/Home/FoodsList.vue
+++ src/components/Home/FoodsList.vue
@@ -3,12 +3,13 @@
     <div class="container">
       <h1 class="title">غذاهای ما</h1>
       <div class="foods-wrapper">
-        <div v-for="(food, index) in foods" :key="index" class="single-food">
+        <div v-for="(food, index) in foodStore.foodsList" :key="index" class="single-food">
           <img :src="food.src" alt="" />
           <div class="img-description">
             <h3>{{ food.name }}</h3>
             <h3>قیمت : {{ food.price }} تومان</h3>
-            <button>سفارش</button>
+            <button @click="foodStore.addOrderNumber(index)">سفارش</button>
+            <p class="mt-3"><span>تعداد سفارش : </span> {{ food.number }}</p>
           </div>
         </div>
       </div>
@@ -17,21 +18,8 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import foods1 from '../../assets/images/gallery-img-1.jpeg'
-import foods2 from '../../assets/images/gallery-img-2.jpeg'
-import foods3 from '../../assets/images/gallery-img-3.jpeg'
-import foods4 from '../../assets/images/gallery-img-4.jpeg'
-import foods5 from '../../assets/images/gallery-img-5.jpeg'
-import foods6 from '../../assets/images/gallery-img-6.jpeg'
-const foods = ref([
-  { name: 'همبرگر', price: 5000, src: foods1 },
-  { name: 'همبرگر', price: 8000, src: foods2 },
-  { name: 'همبرگر', price: 6000, src: foods3 },
-  { name: 'همبرگر', price: 2000, src: foods4 },
-  { name: 'همبرگر', price: 4000, src: foods5 },
-  { name: 'همبرگر', price: 3000, src: foods6 }
-])
+import { useFoodOrderStore } from '@/stores/foodOrder'
+const foodStore = useFoodOrderStore()
 </script>
 
 <style lang="scss" scoped></style>
